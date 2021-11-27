94dbafe4473d86219619aa8a600d53ef303a6f1b
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-27 21:57:27 +0100

    create hello-redis example

---
+++ examples/hello-redis.rs
@@ -0,0 +1,23 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    /*
+     * client::connect asynchronously establishes a TCP connection
+     * for the specified address.
+     * Afterwards a client handle is returned.
+     * The only indication that it is asynchronous is the
+     * .await operator.
+     */
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set a new key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
